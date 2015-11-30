0b43d834f2715b47f23875eedd569753b7486e18
guytepper <guytepper@gmail.com>
2015-11-30 22:19:07 +0200

    navigation works with right+left+enter keys
    needs to fix navigation order - using the order css property

---
+++ js/depre.js
@@ -0,0 +1,110 @@
+var view = {
+  fruits: document.getElementsByClassName('frt-item'),
+  statusElm: document.getElementById('status'),
+  selectedFruits: document.getElementById('selected-fruits'),
+  fruitsDiv: document.getElementById('fruits'),
+  
+  updateStatusText: function() {
+    var status = combination.status;
+    if (status == true) this.statusElm.textContent = 'Great!';
+    else this.statusElm.textContent = 'Bad';
+    this.statusElm.className = status;
+  },
+
+  moveFruit: function(fruit, dest) {
+    this[dest].appendChild(fruit);
+  }
+};
+
+(function() { 
+  Array.prototype.unique = function() {
+    var out = [],
+        obj = {};
+
+    this.forEach(function(value) { 
+      obj[value] = 0; 
+    });
+
+    for ( var i in obj ) {
+      out.push(i);
+    }
+
+    return out;
+  };
+
+  Array.prototype.add = function(fruit) {
+    this.push(fruit);
+    view.updateStatusText();
+  };
+
+  Array.prototype.drop = function(fruit) {
+    this.splice(this.indexOf(fruit), 1);    
+    view.updateStatusText();
+  };
+
+  Array.prototype.checkCombination = function() {
+
+    Array.prototype.hasFruits = function() {
+      return this.indexOf('sweet') != -1 || types.indexOf('sub-acid') != -1 || types.indexOf('acid') != -1;
+    };
+    Array.prototype.hasSweet = function() {
+      return this.indexOf('sweet') != -1;
+    };
+    Array.prototype.hasSubAcid = function() {
+      return this.indexOf('sub-acid') != -1;
+    };
+    Array.prototype.hasAcid = function() {
+      return this.indexOf('acid') != -1;
+    };
+    Array.prototype.hasFat = function() {
+      return this.indexOf('fat') != -1;
+    };
+    Array.prototype.hasMelon = function() {
+      return this.indexOf('melon') != -1;
+    };
+        
+    var types = this.map(function(fruit) {
+      return fruit.type;
+    }).unique();
+
+    if ( types.hasFat() && types.hasFruits() ) return false;
+    if ( types.hasMelon() && types.length > 1 ) return false;
+    if ( types.hasAcid() && types.hasSweet() ) return false;
+    return true; 
+  };
+})();
+
+var combination = {
+  arr: [],
+  combination: false,
+  get status() {
+    this.combination = this.arr.checkCombination();
+    return this.combination;
+  },
+};
+
+// Event Attacher
+Array.prototype.forEach.call(view.fruits, function(fruitDiv) {
+  var fruit = {
+    name : fruitDiv.id,
+    type : fruitDiv.dataset.fruitType
+  };
+  fruitDiv.fruit = fruit;
+  fruitDiv.addEventListener('click', currentFruits(fruitDiv, fruit));
+});
+
+function currentFruits (div, fruit) {
+  return function(event) {
+    if ( div.getAttribute('aria-checked') != 'true' ) {
+      combination.arr.add(fruit);
+      view.moveFruit(div, 'selectedFruits');
+      div.setAttribute('aria-checked', 'true');
+    }
+
+    else {
+      div.setAttribute('aria-checked', 'false');
+      combination.arr.drop(fruit);
+      view.moveFruit(div, 'fruitsDiv');
+    }
+  };    
+}
--- js/keyboard.js
+++ js/keyboard.js
@@ -1,16 +1,52 @@
-window.addEventListener('keydown', function() { 
-  switch(event.which) {
-    case 37:
-      console.log('Left Key Pressed');
-      break;      
-    case 38: 
-      console.log('Up Key Pressed');
-      break;
-    case 39: 
-      console.log('Right Key Pressed');
-      break;
-    case 40:
-      console.log('Down Key Pressed');
-      break;
+function focusController(currentElm) {
+  this.currentElm = currentElm;
+}
+
+focusController.prototype.nextItem = function() {
+  return this.currentElm.nextElementSibling;  
+};
+
+focusController.prototype.previousItem = function() {
+  return this.currentElm.previousElementSibling;
+};
+
+focusController.prototype.selectItem = function() {
+  var event = new MouseEvent('click');
+  this.currentElm.dispatchEvent(event);
+};
+
+function keyAccessibility() {
+  var key = event.which;
+  if ( key == 37 || key == 39 ||key == 13 ) {
+    
+    var currentElm = document.activeElement,
+        self = new focusController(currentElm),
+        nextItem = self.nextItem(),
+        prevItem = self.previousItem();
+    // console.log(prevItem);
+
+    switch(key) {
+      case 37:
+        if ( prevItem ) {
+          currentElm.setAttribute('tabindex', -1);
+          prevItem.setAttribute('tabindex', 0);
+          prevItem.focus();
+        }
+        break;
+      case 39: 
+        if ( nextItem ) {
+          currentElm.setAttribute('tabindex', -1);
+          nextItem.setAttribute('tabindex', 0);
+          nextItem.focus();
+        }
+        break;
+      case 13:
+        self.nextItem().setAttribute('tabindex', 0);
+        self.nextItem().focus();
+        self.selectItem();
+    }
   }
-});
+}
+
+view.fruitsDiv.addEventListener('keydown', keyAccessibility);
+view.selectedFruits.addEventListener('keydown', keyAccessibility);
