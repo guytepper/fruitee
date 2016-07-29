c8a3b8e4a9170144f846529b1d1f29f2f15c11ee
guytepper <guytepper@gmail.com>
2016-07-29 14:23:46 +0300

    Added gulp tasks to generate fruit icons stylesheet

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-var gulp 		   = require('gulp'),
- 	sass 		   = require('gulp-sass'),
-	injectPartials = require('gulp-inject-partials');
+var gulp 		     = require('gulp'),
+ 	sass 		       = require('gulp-sass'),
+	injectPartials = require('gulp-inject-partials'),
+  inlineSvg      = require("./src/js/vendor/gulp-inline-svg");
 
 gulp.task('index', function () {
   return gulp.src('./src/html/index.html')
@@ -12,6 +13,25 @@ gulp.task('index', function () {
 
 gulp.task('sass', function () {
   return gulp.src('./src/sass/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+// converts all svgs to use as inline svgs in css file
+// * modified inline-svg package to prevent encoding
+// * svg-url SASS function handles it instead
+gulp.task('inline-svg', function() {
+  gulp.src('./src/fruit-icons/*.svg')
+    .pipe(inlineSvg({
+      filename: 'fruits.scss',
+      template: './src/fruit-icons/inline-template.mustache'
+    }))
+    .pipe(gulp.dest('./src/sass/'));
+});
+
+// compile fruits.scss to css
+gulp.task('iconify', function() {
+   return gulp.src('./src/sass/fruits.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist/css'));
 });
