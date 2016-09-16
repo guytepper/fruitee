7f283e4501805fdad321785d80cb8fb2beb028ba
Guy Tepper <guytepper@gmail.com>
2016-09-16 11:26:17 +0300

    Created FruitsList module

---
+++ src/js/FruitsList.js
@@ -0,0 +1,5 @@
+export class FruitsList extends Array {
+  constructor() {
+
+  }
+}
---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+import FruitsList from './FruitsList';
+
+let arr = new FruitsList();
+arr.push(3);
+console.log(arr);
