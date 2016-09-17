7f6377cce4ec51bb9408e5610366f8c92ad10065
Guy Tepper <guytepper@gmail.com>
2016-09-17 14:32:12 +0300

    Added types method on fruits list
    With caching

--- src/js/FruitsList.js
+++ src/js/FruitsList.js
@@ -10,7 +10,16 @@ export default class FruitsList extends Array {
       );
     }
 
-    // Push fruit to the array
     super.push(fruit);
+  }
+
+  get types() {
+    // Check if fruits list has changed (cache result if it has)
+    if (this.cachedList != this) {
+      this.cachedList = this;
+      this.typesList = new Set(this.map(fruit => fruit.type));
+    }
+    return this.typesList;
+
   }
 }
