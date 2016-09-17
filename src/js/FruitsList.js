a34c21d78133beee98552e3464130a1b8adfaacf
Guy Tepper <guytepper@gmail.com>
2016-09-17 12:46:48 +0300

    Added push method to FruitsList

--- src/js/FruitsList.js
+++ src/js/FruitsList.js
@@ -1,3 +1,16 @@
-export function OMG() {
-  return true
+// FruitsList acts as an array with additional helper methods
+import Fruit from './Fruit';
+
+export default class FruitsList extends Array {
+  push(fruit) {
+    // Validate that a fruit object is being added to the array
+    if (!(fruit instanceof Fruit)) {
+      throw new TypeError(
+        `Cannot add '${fruit}' to fruits list: not an instance of Fruit.`
+      );
+    }
+
+    // Push fruit to the array
+    super.push(fruit);
+  }
 }
