db3eb715fb93b857a5367673515e15cb5a51603c
guytepper <guytepper@gmail.com>
2016-09-17 17:18:28 +0300

    Made combinations a module

--- src/js/combinations.js
+++ src/js/combinations.js
@@ -2,6 +2,7 @@
 // * Fruit types should be sorted (A-Z), since the array that will be checked
 //   against will be sorted.
 // * When an combination is FALSE - add an info message to explain why it's bad.
+
 let combinations = {};
 
 combinations[['melon']] = {
@@ -13,4 +14,24 @@ combinations[['sweet', 'acid']] = {
   message: ''
 };
 
-export default combinations;
+
+export default class Combination {
+  static check(types) {
+    if ( types.length === 1 ) {
+      return { combination: true }
+    }
+
+    if ( types.includes('melon') && types.length > 1) {
+      return { combination: false, message: 'Melons are best eaten alone.'}
+    }
+
+    const combination = combinations[this.types];
+
+    if ( combination ) {
+      return combination;
+    }
+
+    console.log('Huston, we have a problem - NO COMBINATION WAS FOUND!!');
+
+  }
+}
