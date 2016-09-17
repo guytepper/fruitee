748a41798cd7ba2de20b308a18a08fc572adbb5b
Guy Tepper <guytepper@gmail.com>
2016-09-17 16:19:25 +0300

    Initial combinations file

---
+++ src/js/combinations.js
@@ -0,0 +1,9 @@
+// This file contains all the possible fruit combinations.
+// * Fruit types 
+// * When an combination is FALSE - add an info message to explain why it's bad.
+
+let combinations = {};
+
+combinations[['melon']] = {
+  combination: true
+};
