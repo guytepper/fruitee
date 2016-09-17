3f68c99ef66e7227cf37c44368415cfa142d0692
guytepper <guytepper@gmail.com>
2016-09-17 17:20:35 +0300

    Some testing on the client

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,16 @@
 import Fruit from './Fruit';
 import FruitsList from './FruitsList';
-import combinations from './combinations';
+import Combination from './Combination';
 
 let arr = new FruitsList();
 let watermelon = new Fruit('Watermelon', 'melon');
+let orange = new Fruit('Orange', 'acid');
+
 arr.push(watermelon);
-console.log(arr.melon);
+arr.push(orange);
 
 // console.log(combinations);
 //
 // console.log( combinations[['sweet', 'acid']] );
+
+console.log(Combination.check(arr.types));
