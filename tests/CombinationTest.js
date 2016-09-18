1739b0f91178567fb302ec70e2343d1fca6e86ab
Guy Tepper <guytepper@gmail.com>
2016-09-18 21:03:10 +0300

    Fixed mono-melon test

--- tests/CombinationTest.js
+++ tests/CombinationTest.js
@@ -4,11 +4,12 @@ import FruitsList from '../src/js/FruitsList';
 import Combination from '../src/js/Combination';
 
 describe("Combination", () => {
-  let fruits = new FruitsList();
-  let watermelon = new Fruit('Watermelon', 'melon');
-  fruits.push(watermelon);
-
   it("doesn't combine melons and fruits", () => {
+    let fruits = new FruitsList();
+    let watermelon = new Fruit('Watermelon', 'melon');
+    let banana = new Fruit('Banana', 'sweet');
+    fruits.push(watermelon);
+    fruits.push(banana);
     let result = Combination.check(fruits);
     expect(result.combination).toBe(false);
   });
