24ebd7a0bb2b9c557d066d0b9704037279c87edb
Guy Tepper <guytepper@gmail.com>
2016-09-23 16:15:41 +0300

    Added generateMessage method

--- src/js/Combination.js
+++ src/js/Combination.js
@@ -1,4 +1,10 @@
 export default class Combination {
+  static generateMessage(list, ...types) {
+    const fruitsA = list.getFruitsOfType({ type: types[0], onlyNames: true });
+    const fruitsB = list.getFruitsOfType({ type: types[1], onlyNames: true });
+    return `Combining ${types[0]} fruits (${fruitsA}) and ${types[1]} fruits (${fruitsB}) is not optimal.`;
+  }
+
   static check(list) {
     const types = list.types;
     let result = { combination: true, fruits: list };
