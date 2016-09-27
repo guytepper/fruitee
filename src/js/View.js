7337695fab394803cfb6e3bca44373f68e1533c9
guytepper <guytepper@gmail.com>
2016-09-27 19:06:39 +0300

    Created basic Vue instance

--- src/js/View.js
+++ src/js/View.js
@@ -1,3 +1,19 @@
-export new Vue({
-	el: '#app',
+import Fruit from './Fruit';
+import FruitsList from './FruitsList';
+import Combination from './Combination';
+
+export var vm = new Vue({
+  el: '#app',
+  data: {
+    list: new FruitsList(),
+    fruits: {}
+  },
+  created: function() {
+    // Init fruits object base on DOM elements
+    let fruits = document.getElementsByClassName('frt-item');
+    Array.prototype.forEach.call(fruits, (fruit) => {
+      let type = fruit.getAttribute('data-fruit-type');
+      this.fruits[fruit.id] = new Fruit(fruit.id, type);
+    });
+  }
 });
