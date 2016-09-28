354f8d380ac0f9ec0fac69ea36ae422f4faa9a5b
Guy Tepper <guytepper@gmail.com>
2016-09-28 13:16:01 +0300

    Added fruits list binding

--- src/js/View.js
+++ src/js/View.js
@@ -9,9 +9,9 @@ const FruitsComponent = Vue.extend({
 });
 
 Vue.component('my-fruit', {
-  props: ['fruit'],
+  props: ['fruit', 'list'],
   template: `
-    <div class="frt-item" v-bind:id="fruit.name" v-bind:data-fruit-type="fruit.type">
+    <div v-on:click="list.push(fruit)" class="frt-item" v-bind:id="fruit.name" v-bind:data-fruit-type="fruit.type">
     <label v-bind:for="fruit.name">{{ fruit.name }}</label></div>
   `
 });
