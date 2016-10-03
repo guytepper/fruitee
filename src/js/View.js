402a06d8211d6784b26856d0c322d47500593c10
Guy Tepper <guytepper@gmail.com>
2016-10-03 19:37:57 +0300

    Added selectedFruits prop
    For more convenient vue.js rendering

--- src/js/View.js
+++ src/js/View.js
@@ -7,30 +7,38 @@ export const vm = new Vue({
   el: '#app',
   data: {
     fruitsList: new FruitsList(),
+    selectedFruits: [],
     fruits: [],
     message: '',
   },
   created: function() {
     // Map fruits dictionary to Fruit object array
+    let count = 0;
     for (let prop in fruits) {
       let fruit = fruits[prop];
-      this.fruits.push(new Fruit(fruit.name, fruit.type));
+      this.fruits.push({
+       id: count,
+       data: new Fruit(fruit.name, fruit.type)
+      });
+      count++;
     }
   },
   methods: {
     selectFruit: function(fruit, index) {
-      this.fruitsList.push(fruit);
-      // this.fruits.splice(index, 1);
+      console.log(fruit);
+      this.fruitsList.push(fruit.data);
+      this.selectedFruits.push(fruit);
       this.fruits.splice(this.fruits.indexOf(fruit), 1);
     },
     removeFruit: function(fruit) {
-      this.fruitsList.remove(fruit);
+      this.fruitsList.remove(fruit.data);
+      this.selectedFruits.splice(this.selectedFruits.indexOf(fruit), 1);
       this.fruits.push(fruit);
     }
   },
   watch: {
-    'fruitsList.list': function() {
-      this.message = Combination.check(this.fruitsList).message;
+    'fruitsList.list': function(list) {
+      this.message = Combination.check(this.fruitsList).message;      
     }
   }
 });
