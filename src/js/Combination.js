f7e76cb505eca3d6392546bfc7fb3a1d6385a7e9
Guy Tepper <guytepper@gmail.com>
2016-10-07 12:08:41 +0300

    2 fat types combination (#31)
    * Check for more than 2 fats combination
    
    * Changed types getter to uniqueTypes
    
    * Added types getter on fruits list
    
    * Tests for new getters
    
    * Function explnations
    
    * Check if more than one fat exists in the array
    
    * Fixed variable namings
    
    * Added missing semicolon
    
    * Added 2 fat types test
    
    * Added types to has function
    
    * Changed combination check method
    
    * getFruitsOfType's default onlyNames parameter to true
    
    * Removed unused type variable
    
    * Added fat combining message
    
    * Removed combination log message

--- src/js/Combination.js
+++ src/js/Combination.js
@@ -1,41 +1,46 @@
 export default class Combination {
   static generateMessage(list, ...types) {
-    const fruitsA = list.getFruitsOfType({ type: types[0], onlyNames: true });
-    const fruitsB = list.getFruitsOfType({ type: types[1], onlyNames: true });
+    const fruitsA = list.getFruitsOfType({ type: types[0] });
+    const fruitsB = list.getFruitsOfType({ type: types[1] });
     return `Combining ${types[0]}s (${fruitsA}) and ${types[1]}s (${fruitsB}) is not optimal.`;
   }
 
   static check(fruitsList) {
-    const types = fruitsList.types;
-    let combination = { status: true, fruits: fruitsList.list, message: '' };
+    const list = fruitsList.list;
+    let combination = { status: true, fruits: list, message: '' };
 
-    if ( types.includes('melon') && fruitsList.list.length > 1) {
+    if ( fruitsList.has('melon') && list.length > 1) {
       combination.status = false;
       combination.message = '';
       return combination;
     }
 
-    if ( types.includes('sweet') && types.includes('acid') ) {
+    if ( fruitsList.has('sweet') && fruitsList.has('acid') ) {
       combination.status = false;
       combination.message = this.generateMessage(fruitsList, 'sweet', 'acid');
       return combination;
     }
 
-    if ( types.includes('sweet') && types.includes('acid-veggie') ) {
+    if ( fruitsList.has('sweet') && fruitsList.has('acid-veggie') ) {
       combination.status = false;
       combination.message = this.generateMessage(fruitsList, 'sweet', 'acid-veggie');
       return combination;
     }
 
-    if ( types.includes('sweet') && types.includes('fat') ) {
+    if ( fruitsList.has('sweet') && fruitsList.has('fat') ) {
       combination.status = false;
       combination.message = this.generateMessage(fruitsList, 'sweet', 'fat');
       return combination;
     }
 
-    return combination;
+    if ( fruitsList.has('fats') ) {
+      combination.status = false;
+      // TODO: Add fat names
+      const fatNames = fruitsList.getFruitsOfType({ type: 'fat' });
+      combination.message = `Combining more than one fat (${fatNames}) in a meal is not optimal.`;
+      return combination;
+    }
 
-    console.log('%c No combination exists! Needs to take care of that huston!',
-                'background: #222; color: #bada55');
+    return combination;
   }
 }
