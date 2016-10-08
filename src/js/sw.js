acf19e0a4f19094f063c3a0c3f5491b1ad7593f1
Guy Tepper <guytepper@gmail.com>
2016-10-08 16:20:02 +0300

    Added fetch event handler

--- src/js/sw.js
+++ src/js/sw.js
@@ -11,3 +11,12 @@ self.addEventListener('install', event => {
     })
   );
 });
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  );
+});
